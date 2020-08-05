10e683b20184142c1ca33ab549c6c78cbab003b2
A.J. Bale <abalejr@gmail.com>
2020-08-04 23:49:59 -0500

    Download single page

--- peeper.py
+++ peeper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 
 url = "https://www.omgbeaupeep.com/comics/Avatar_The_Last_Airbender/001/1/"
@@ -10,4 +10,9 @@ html = urlopen(url)
 soup = BeautifulSoup(html, 'lxml')
 
 page = soup.find_all('img', {"class": "picture"})[0]
-print(page.get('src'))
+page_relative_src = page.get('src')
+
+page_src = "https://www.omgbeaupeep.com/comics/" + page_relative_src
+encoded_page_src = page_src.replace(" ", "%20")
+
+urlretrieve(encoded_page_src, "page1.jpg")
