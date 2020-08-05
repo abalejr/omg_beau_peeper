7b45b99f804d139be0c5057e9502d55e6e6985fb
A.J. Bale <abalejr@gmail.com>
2020-08-05 00:00:52 -0500

    Download image and convert it to pdf

--- peeper.py
+++ peeper.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
+from PIL import Image
 
 url = "https://www.omgbeaupeep.com/comics/Avatar_The_Last_Airbender/001/1/"
 html = urlopen(url)
@@ -16,3 +17,7 @@ page_src = "https://www.omgbeaupeep.com/comics/" + page_relative_src
 encoded_page_src = page_src.replace(" ", "%20")
 
 urlretrieve(encoded_page_src, "page1.jpg")
+
+image1 = Image.open( "page1.jpg" )
+im1 = image1.convert('RGB')
+im1.save("page1.pdf")
